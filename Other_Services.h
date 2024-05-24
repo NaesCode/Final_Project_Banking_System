ac5d4a5d610ef4d56aefc3ad5180a08047ab969c
Richard T <seanrichardobeso.tadiamon@gmail.com>
2024-05-24 20:30:10 +0800

    Add files via upload

---
+++ Other_Services.h
@@ -0,0 +1,7 @@
+// Other_Services.h
+#ifndef OTHER_SERVICES_H
+#define OTHER_SERVICES_H
+
+void record_transaction(const char *username, const char *transaction_type, float amount);
+void display_transaction_history(char* username, char* passcode, int *loggedIn);
+#endif
